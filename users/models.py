39c4928337dd73ae2dab759da5da3ee299483d2c
kamcho <njokevin9@gmail.com>
2026-06-17 15:20:33 +0300

    verify

--- users/models.py
+++ users/models.py
@@ -38,6 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         ('Radiographer', 'Radiographer'),
         ('Procurement Officer', 'Procurement Officer'),
         ('SHA Manager', 'SHA Manager'),
+        ('Mortuary Officer', 'Mortuary Officer'),
     ]
     id_number = models.CharField(max_length=20, unique=True)
     first_name = models.CharField(max_length=50, blank=True, null=True)
