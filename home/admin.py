e85634d4e4bbd3c8e9a6ceea092743edee353a56
kamcho <njokevin9@gmail.com>
2026-06-17 13:14:37 +0300

    verify

--- home/admin.py
+++ home/admin.py
@@ -1,15 +1,110 @@
 from django.contrib import admin
-from .models import Patient, Departments, PatientQue, Visit, TriageEntry, Consultation, ConsultationNotes, EmergencyContact, Prescription
+from .models import (
+    Patient,
+    Departments,
+    PatientQue,
+    Visit,
+    TriageEntry,
+    Consultation,
+    ConsultationNotes,
+    EmergencyContact,
+    Prescription,
+    PrescriptionItem,
+)
+
+
+class PrescriptionItemInline(admin.TabularInline):
+    model = PrescriptionItem
+    extra = 0
+    fields = (
+        'medication',
+        'dose_count',
+        'dose_unit',
+        'frequency',
+        'number_of_days',
+        'quantity',
+        'instructions',
+        'dispensed',
+        'dispensed_at',
+        'dispensed_by',
+    )
+    readonly_fields = ('dispensed_at',)
+    autocomplete_fields = ('medication',)
+    show_change_link = True
+
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'patient',
+        'visit',
+        'prescribed_by',
+        'prescribed_at',
+        'status',
+        'item_count',
+    )
+    list_filter = ('status', 'prescribed_at')
+    search_fields = (
+        'patient__first_name',
+        'patient__last_name',
+        'diagnosis',
+        'visit__id',
+    )
+    readonly_fields = ('prescribed_at',)
+    autocomplete_fields = ('patient', 'visit', 'invoice', 'prescribed_by')
+    inlines = [PrescriptionItemInline]
+    fieldsets = (
+        (None, {
+            'fields': ('patient', 'visit', 'invoice', 'prescribed_by', 'prescribed_at', 'status'),
+        }),
+        ('Clinical', {
+            'fields': ('diagnosis', 'notes'),
+        }),
+    )
+
+    @admin.display(description='Items')
+    def item_count(self, obj):
+        return obj.items.count()
+
+
+@admin.register(PrescriptionItem)
+class PrescriptionItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'prescription',
+        'medication',
+        'frequency',
+        'quantity',
+        'dispensed',
+    )
+    list_filter = ('dispensed', 'frequency')
+    search_fields = (
+        'prescription__patient__first_name',
+        'prescription__patient__last_name',
+        'medication__name',
+    )
+    autocomplete_fields = ('prescription', 'medication', 'dispensed_by')
+
+
 # Register your models here.
-admin.site.register(Patient)
-admin.site.register(Departments)
 admin.site.register(PatientQue)
-admin.site.register(Visit)
 admin.site.register(TriageEntry)
 admin.site.register(Consultation)
 admin.site.register(ConsultationNotes)
 
-admin.site.register(Prescription)
+
+@admin.register(Departments)
+class DepartmentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation')
+    search_fields = ('name', 'abbreviation')
+
+
+@admin.register(Visit)
+class VisitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'patient', 'visit_date', 'visit_type', 'is_active')
+    list_filter = ('visit_type', 'is_active', 'visit_date')
+    search_fields = ('patient__first_name', 'patient__last_name', 'id')
+    autocomplete_fields = ('patient',)
+
 class EmergencyContactInline(admin.TabularInline):
     model = EmergencyContact
     extra = 1
@@ -40,6 +135,7 @@ class EmergencyContactAdmin(admin.ModelAdmin):
 
 
 # Add EmergencyContact inline to Patient admin
+@admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'age', 'phone', 'location', 'gender', 'created_at']
     list_filter = ['gender', 'created_at']
@@ -51,7 +147,3 @@ class PatientAdmin(admin.ModelAdmin):
         if not change:
             obj.created_by = request.user
         super().save_model(request, obj, form, change)
-
-# Unregister and re-register Patient with new admin
-admin.site.unregister(Patient)
-admin.site.register(Patient, PatientAdmin)
--- inpatient/admin.py
+++ inpatient/admin.py
@@ -1,5 +1,59 @@
 from django.contrib import admin
-from .models import Ward, Bed, Admission, MedicationChart, ServiceAdmissionLink
+from .models import (
+    Ward,
+    Bed,
+    Admission,
+    MedicationChart,
+    ServiceAdmissionLink,
+    InpatientConsumable,
+)
+
+
+class MedicationChartInline(admin.TabularInline):
+    model = MedicationChart
+    extra = 0
+    verbose_name = 'Medication chart entry'
+    verbose_name_plural = 'Medication chart'
+    fields = (
+        'item',
+        'administration_type',
+        'dose_count',
+        'frequency',
+        'quantity',
+        'duration_days',
+        'total_quantity',
+        'quantity_dispensed',
+        'is_dispensed',
+        'is_active',
+        'prescribed_at',
+        'prescribed_by',
+        'instructions',
+    )
+    readonly_fields = ('prescribed_at',)
+    autocomplete_fields = ('item', 'prescribed_by')
+    show_change_link = True
+
+
+class InpatientConsumableInline(admin.TabularInline):
+    model = InpatientConsumable
+    extra = 0
+    verbose_name = 'Prescribed consumable'
+    verbose_name_plural = 'Prescribed consumables'
+    fields = (
+        'item',
+        'quantity',
+        'total_quantity',
+        'quantity_dispensed',
+        'is_dispensed',
+        'prescribed_at',
+        'prescribed_by',
+        'request_location',
+        'instructions',
+    )
+    readonly_fields = ('prescribed_at',)
+    autocomplete_fields = ('item', 'prescribed_by', 'request_location')
+    show_change_link = True
+
 
 @admin.register(Ward)
 class WardAdmin(admin.ModelAdmin):
@@ -10,17 +64,51 @@ class WardAdmin(admin.ModelAdmin):
 class BedAdmin(admin.ModelAdmin):
     list_display = ('bed_number', 'ward', 'is_occupied', 'bed_type')
     list_filter = ('ward', 'is_occupied', 'bed_type')
+    search_fields = ('bed_number', 'ward__name')
 
 @admin.register(Admission)
 class AdmissionAdmin(admin.ModelAdmin):
-    list_display = ('patient', 'bed', 'admitted_at', 'status')
+    list_display = ('patient', 'bed', 'admitted_at', 'status', 'medication_count', 'consumable_count')
     list_filter = ('status', 'admitted_at', 'bed__ward')
     search_fields = ('patient__first_name', 'patient__last_name', 'provisional_diagnosis')
+    readonly_fields = ('admitted_at',)
+    autocomplete_fields = ('patient', 'visit', 'bed', 'admitted_by', 'discharged_by')
+    inlines = [MedicationChartInline, InpatientConsumableInline]
+    fieldsets = (
+        (None, {
+            'fields': ('patient', 'visit', 'bed', 'status', 'admitted_at', 'admitted_by'),
+        }),
+        ('Clinical', {
+            'fields': ('provisional_diagnosis', 'final_diagnosis', 'discharge_summary'),
+        }),
+        ('Discharge', {
+            'fields': ('discharged_at', 'discharged_by'),
+            'classes': ('collapse',),
+        }),
+    )
+
+    @admin.display(description='Chart meds')
+    def medication_count(self, obj):
+        return obj.medications.count()
+
+    @admin.display(description='Consumables')
+    def consumable_count(self, obj):
+        return obj.consumables.count()
 
 @admin.register(MedicationChart)
 class MedicationChartAdmin(admin.ModelAdmin):
-    list_display = ('item', 'admission', 'dosage', 'frequency', 'is_administered')
-    list_filter = ('is_administered', 'prescribed_at')
+    list_display = ('item', 'admission', 'frequency', 'quantity', 'is_dispensed', 'is_administered')
+    list_filter = ('is_dispensed', 'is_administered', 'is_active', 'prescribed_at')
+    search_fields = ('item__name', 'admission__patient__first_name', 'admission__patient__last_name')
+    autocomplete_fields = ('admission', 'item', 'prescribed_by')
+
+
+@admin.register(InpatientConsumable)
+class InpatientConsumableAdmin(admin.ModelAdmin):
+    list_display = ('item', 'admission', 'quantity', 'is_dispensed', 'prescribed_at')
+    list_filter = ('is_dispensed', 'prescribed_at')
+    search_fields = ('item__name', 'admission__patient__first_name', 'admission__patient__last_name')
+    autocomplete_fields = ('admission', 'item', 'prescribed_by')
 
 @admin.register(ServiceAdmissionLink)
 class ServiceAdmissionLinkAdmin(admin.ModelAdmin):
